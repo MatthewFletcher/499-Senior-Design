274ea080ac4db5046cbafca4af5c387ea7f134b8
Sameera <sfw0007@uah.edu>
2019-09-01 16:45:06 -0500

    Began front end work. Tried to import various graphing libraries, having issues.

---
+++ src/FrontEnd/graphs.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import statsmodels
+
+# plt.plot(x, y)
+# plt.title("Statistical Graph")
+# plt.xlabel("x")
+# plt.ylabel("y")
+# plt.show()
+
+# sample_data = pd.read_csv('FrequencyDataTest.csv')
+# sample_data2 = pd.read_csv('IntervalDataTest.csv')
+# sample_data3 = pd.read_csv('OrdinalDataTest.csv')
