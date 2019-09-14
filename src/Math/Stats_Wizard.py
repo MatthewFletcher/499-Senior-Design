81a2c663ffce82467dbe481f357a73383c699d3a
Fletcher <bassmattf@gmail.com>
2019-09-14 09:53:33 -0500

    Fleshing out functions

--- src/Math/Stats_Wizard.py
+++ src/Math/Stats_Wizard.py
@@ -2,4 +2,23 @@ import numpy as np
 import statistics as s
 
 
+Class Regression:
+    def __init__(self, a):
+        self.data = a
+
+    def linear(self):
+        '''takes in a set of xy coordinates and calculates linear regression
+        model 
+        Parameters: None
+        Returns: Dictionary d
+            d['slope'] = m
+            d['y_int'] = b
+            d['fit'] = score (TBD)
+        '''
+        
+        
+    
+    def normal(self):
+        pass
+
 
