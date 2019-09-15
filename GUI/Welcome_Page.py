93cfd8a8b1ada7954a0dc2b19c411bcc3898fb97
Jada Blankenship <30299670+jada4569@users.noreply.github.com>
2019-09-15 16:02:27 -0500

    Working on start page

--- GUI/Welcome_Page.py
+++ GUI/Welcome_Page.py
@@ -1,26 +1,48 @@
 from PyQt5.QtWidgets import (QApplication,
-QLabel,  QPushButton, QGridLayout, QWidget)
+QLabel,  QPushButton, QGridLayout, QWidget, 
+QVBoxLayout, QGroupBox)
 from PyQt5.QtGui import QPixmap
+import sys
 
 class StartPage(QWidget):
     def __init__(self):
         super().__init__()
 
-        app = QApplication([])
-        app.setStyle("Fusion")
-        window = QWidget()
+        self.title = "Stats Wiz Start Page"
+        self.left = 10
+        self.top = 10
+        self.width = 500
+        self.height = 500
+        self.initUi()
 
-        label = QLabel
-        label.setPixmap(QLabel(), QPixmap("StatsLogo1.png"))
+    def initUi(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.createGridLayout()
 
+        pixmap = QPixmap("StatsLogo1.png")
+
+        lbl = QLabel(self)
+        lbl.setPixmap(pixmap)
+
+        windowLayout = QVBoxLayout()
+        windowLayout.addWidget(lbl)
+        self.setLayout(windowLayout)
+
+        self.show()
+
+    def createGridLayout(self):
         layout = QGridLayout()
-        layout.addWidget(QPushButton("Import CSV File"))
-        layout.addWidget(QPushButton("Manual Input"))
-        layout.addWidget(QPushButton("Help"))
+        layout.setColumnStretch(1, 4)
+        layout.setColumnStretch(2, 4)
 
-        window.setWindowTitle("Stats Wiz Start Page")
-        window.setLayout(layout)
-        window.show()
-        app.exec_()
+        layout.addWidget(QPushButton("Import CSV File"), 2, 2)
+        layout.addWidget(QPushButton("Manual Input"), 2, 3)
+        self.setLayout(layout)
+
+
     
-StartPage()
+app = QApplication(sys.argv)
+startPage = StartPage()
+startPage.show()
+app.exec()
