49998318d8324950529197a3597fdc485f72feca
Fletcher <bassmattf@gmail.com>
2019-09-16 12:17:05 -0500

    more functions added

--- src/Math/Stats_Wizard.py
+++ src/Math/Stats_Wizard.py
@@ -2,9 +2,35 @@ import numpy as np
 import statistics as s
 
 
-Class Regression:
+class Regression:
     def __init__(self, a):
         self.data = a
+        self.cols = a.T
+
+    def range(self):
+        return [np.amax(col) - np.amin(col) for col in self.data.T]
+    
+    def mean(self):
+        return np.avg(self.data)
+
+    def mode(self):
+        pass
+
+    def median(self):
+        return np.median(self.data)
+
+    def var(self):
+        return np.var(self.data)
+
+    def stddev(self):
+        return np.std(self.data)        
+
+    def pearsonR(self):
+        # http://onlinestatbook.com/2/describing_bivariate_data/calculation.html
+        
+
+        return 1
+
 
     def linear(self):
         '''takes in a set of xy coordinates and calculates linear regression
@@ -14,8 +40,11 @@ Class Regression:
             d['slope'] = m
             d['y_int'] = b
             d['fit'] = score (TBD)
+
+        # Reference: http://onlinestatbook.com/2/regression/intro.html
         '''
-        
+        pass 
+
         
     
     def normal(self):
