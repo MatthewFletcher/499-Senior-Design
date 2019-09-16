b2ac9ef1db2ea0f35d0c21334ee123cb77f70449
Miguel <mao0008@uah.edu>
2019-09-15 21:10:50 -0500

    created function for rows and columns
    User to specify rows and columns.
    As list variables. example: [0,0] to [6,2]

--- src/csvtools/UserSelect.py
+++ src/csvtools/UserSelect.py
@@ -1,12 +1,55 @@
-#For statistical analyses the user may specify certain rows and columns within the rows, 
-#or columns and rows within the columns to perform the analyses on. 
-# The selection must be appropriate for the type of statistic to be calculated.
+import pandas as pd
+import numpy as np
+import sys
 
-#As a user, I want to be able to select the rows and columns to be analyzed and graphed so I can better understand my data.
+def selection(df,choice, pointA, pointB):
+    if(choice==1):
+        newDF=modify(df,pointA, pointB)
+        return newDF
+    else:
+        return df
 
-def Selection():
-    row=input("Enter row ")
-    column=input("Enter column ")
-    return row,column
+#modify the size of the data based on user. 
+def modify(df, pointA, pointB):
+    ptA, ptB=testPT(pointA, pointB)
+    
+    #get data specified
+    row=[i for i in np.arange(ptA[0],ptB[0]+1)]
+    col=[i for i in np.arange(ptA[1],ptB[1]+1)]
+    newDF=myinfo.iloc[row,col]
+    return newDF
 
 
+def testPT(ptA,ptB):
+    #test to put point higher in case of inversion
+    if(ptA[0]>ptB[0]):
+        ptA[0], ptB[0]=ptB[0],ptA[0]
+    if(ptA[1]>ptB[1]):
+        ptA[1],ptB[1]=ptB[1],ptA[1]
+    return ptA, ptB
+    
+# #Testing Data**
+# print("Starting...\n")
+# fn="TestData/FrequencyDataTest.csv"
+# #fn="TestData/IntervalDataTest.csv"
+# #fn="TestData/OrdinalDataTest.csv"
+# f = open(fn)
+# myinfo = pd.read_csv(f)
+# print(myinfo)
+
+# print("\n\ntype: "+ str(type(myinfo)))
+# print("modify-----\n\n\n")
+
+# #Jada plans to use list var.
+# #[row, col]
+# ptA=[3,1]#default pointA
+# ptB=[5,2]#default pointB ...todo remove later
+
+# #if 1-to specify data
+# #else-use all data so no change
+# test=selection(myinfo, 1, ptA, ptB)
+
+# print(test)
+# print("\n\ntype: "+ str(type(test)))
+# print("End--\n")
+# #**Testing Data
