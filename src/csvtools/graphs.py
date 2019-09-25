ec831c2be8765663bdb1d9a0af5b3f6167a6dd5f
Sameera <sfw0007@uah.edu>
2019-09-25 17:00:06 -0500

    Updating my graphs function to graph frequency, ordinal, and interval data

--- src/csvtools/graphs.py
+++ src/csvtools/graphs.py
@@ -6,21 +6,38 @@ import csv
 import CSV_Wizard
 import UserSelect
 
-# This function will graph the data imported by default
-# without any restrictions.
+# This function will graph the functional or interval data with no restrictions
 def graph(df):
-    x = df[" Expected Freq."]
-    y = df[" Actual Freq."]
+    headers = list(df.columns.values)
+    print(df)
+
+    #for title in headers:
+
+    x = df[headers[1]]
+    y = df[headers[2]]
     plt.title("Frequency Data")
-    plt.xlabel("Expected Frequency")
-    plt.ylabel("Actual Frequency")
+    plt.xlabel(headers[1])
+    plt.ylabel(headers[2])
 
     # Plot the points using matplotlib
     plt.plot(x, y)
     plt.show()
 
+def ordinal(df):
+    headers = list(df.columns.values)
+    # Remove the Question # column
+    headers.pop(0)
+    x = headers
+    y = []
+    for header in headers:
+        y.append(df[header].sum())
+
+    plt.bar(x, y)
+    plt.show()
+    print(y)
+
 if __name__ == '__main__':
     myinfo = CSV_Wizard.openFile("../../TestData/FrequencyDataTest.csv")
     #contains numpy array
     d = myinfo[0]
-    graph(d)
+    ordinal(d)
