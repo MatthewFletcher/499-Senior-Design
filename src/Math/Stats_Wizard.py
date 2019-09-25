d9aa028524a530f93811872f67a03688d4b2e96d
Fletcher <bassmattf@gmail.com>
2019-09-25 17:03:42 -0500

    Added linear regression calculation

--- src/Math/Stats_Wizard.py
+++ src/Math/Stats_Wizard.py
@@ -1,11 +1,15 @@
 import numpy as np
 import statistics as s
-
+import math as ma
 
 class Regression:
-    def __init__(self, a):
+    def __init__(self, a, x_col = 0, y_col = 4):
         self.data = a
         self.cols = a.T
+        
+        #TODO make this less janky
+        self.xcol = a.iloc[:,x_col]
+        self.ycol = a.iloc[:,y_col]
 
     def range(self):
         return [np.amax(col) - np.amin(col) for col in self.data.T]
@@ -27,14 +31,34 @@ class Regression:
 
     def pearsonR(self):
         # http://onlinestatbook.com/2/describing_bivariate_data/calculation.html
+        x = self.xcol
+        y = self.ycol
+        
+        x_mean = np.mean(x)
+        y_mean = np.mean(y)
+        def num(x,y):
+            numsum = 0
+            for m,n in zip(x,y):
+                numsum += ((m-np.mean(x)) * (n - np.mean(y)))
+            return numsum
+
+        def den(x,y):
+            x_sum = 0
+            y_sum = 0
+
+            for m,n in zip(x,y):
+                x_sum += (m - x_mean)**2
+                y_sum += (m - y_mean)**2
+            den = ma.sqrt(x_sum * y_sum)
+            return den
         
 
-        return 1
+        return num(x,y)/den(x,y)
 
 
     def linear(self):
-        '''takes in a set of xy coordinates and calculates linear regression
-        model 
+        '''
+        Calculates the line of best fit for 2 vectors of data
         Parameters: None
         Returns: Dictionary d
             d['slope'] = m
@@ -43,9 +67,27 @@ class Regression:
 
         # Reference: http://onlinestatbook.com/2/regression/intro.html
         '''
-        pass 
 
+        #Save me some typing, set columns to single variables 
+        x = self.xcol
+        y = self.ycol
+
+        #Save more typing, calculate standard deviation
+        x_std = np.std(self.ycol)
+        y_std = np.std(self.ycol)
         
+        #Calculate the y intercept
+        b = self.pearsonR() * y_std / x_std
+
+        #Calculate the slope
+        m = np.mean(y) - b * np.mean(x)
+    
+        #Creation of dictionary for returning
+        d = {}
+        d['slope'] = m
+        d['y_int'] = b
+
+        return d 
     
     def normal(self):
         pass
