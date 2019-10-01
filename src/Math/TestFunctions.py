c9ffe43574d565e40d161a7a73b6eed3f3662671
Miguel <mao0008@uah.edu>
2019-09-30 23:28:30 -0500

    No Change to Function just test

--- src/Math/TestFunctions.py
+++ src/Math/TestFunctions.py
@@ -1,14 +1,14 @@
 import pandas as pd
 import numpy as np
 import sys
+import matplotlib.pyplot as plt
 
  #dont use function literally add and divide
 def mean(df):
     #test=np.mean(df) Check: match
     #print(test)
-
     #Add each columns values total
-    newDF=df.append(df.sum(numeric_only=True)/len(df),ignore_index=True)
+    newDF=df.append(df.sum(numeric_only=True)/len(df),ignore_index=True)  #newDF=pd.DataFrame(df.sum(numeric_only=True)/len(df))
     #retrive the last row which is the mean result.
     newDF=newDF[len(newDF)-1:]
     return newDF
@@ -43,8 +43,23 @@ ptB=[5,2]#default pointB ...todo remove later
 test=mean(myinfo)
 
 
+# headers = list(test.columns.values)
+# print("----------@")
+# print(headers)
 
-print(test)
+# # #for title in headers:
+
+# x = test[headers[1]]
+# y = test[headers[2]]
+# print(x)
+# print(y)
+# plt.scatter(30,30)
+# plt.scatter(20,70)
+# plt.scatter(x,y)
+# plt.show()
+
+
+
 print("\n\ntype: "+ str(type(test)))
 print("End--\n")
 # #**Testing Data
