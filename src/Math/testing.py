5ee7c88b0d3849afc17f43874dd18b9557ff5718
Fletcher <bassmattf@gmail.com>
2019-10-02 13:26:36 -0500

    changing testing stuff

--- src/Math/testing.py
+++ src/Math/testing.py
@@ -1,1 +1,14 @@
-print("Test")
+#!/usr/bin/env python3
+
+import CSV_Wizard as c
+import numpy as np
+import Stats_Wizard as s
+import pandas as pd
+
+
+df = c.openFile("../../TestData/sathead.csv")[0]
+
+#r = s.Regression(df)
+
+
+
