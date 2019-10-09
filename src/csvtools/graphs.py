cc7fdc6de509789008fc65a0975d5391c72bc699
Sameera <sfw0007@uah.edu>
2019-10-09 11:05:19 -0500

    Handled option of graphing frequency or interval data as a pie chart

--- src/csvtools/graphs.py
+++ src/csvtools/graphs.py
@@ -40,21 +40,25 @@ def freqorint_vbar(df):
     plt.show()
 
 # This function will graph the frequency or interval data as a pie chart
-# def frequency_pie(df):
-#     headers = list(df.columns.values)
-#
-#     x = df[headers[1]]
-#     y = df[headers[2]]
-#     explode = []
-#     plt.xlabel(headers[1])
-#     plt.ylabel(headers[2])
-#
-#     explode.append(0)
-#     colors = ['royalblue', 'periwinkle']
-#
-#     # Plot the points using matplotlib
-#     plt.pie(y, explode=explode, labels=x, colors=colors, autopct='%1.1f%%', shadow=False, startangle=0)
-#     plt.show()
+def frequency_pie(df):
+    headers = list(df.columns.values)
+    # Remove the Question # column
+    headers.pop(0)
+    x = headers
+    y = []
+    explode = []
+    for header in headers:
+        y.append(df[header].sum())
+        explode.append(0)
+
+    # Data to plot
+    colors = ['gold', 'yellowgreen']
+
+    # Plot
+    plt.pie(y, explode=explode, labels=x, colors=colors,
+            autopct='%1.1f%%', shadow=False, startangle=0)
+    plt.axis('equal')
+    plt.show()
 
 # This function will graph the ordinal data as a vertical bar graph
 def ordinal_vbar(df):
@@ -106,7 +110,7 @@ def ordinal_pie(df):
 
 # Testing purposes
 if __name__ == '__main__':
-    myinfo = CSV_Wizard.openFile("../../TestData/OrdinalDataTest.csv")
+    myinfo = CSV_Wizard.openFile("../../TestData/IntervalDataTest.csv")
     #contains numpy array
     d = myinfo[0]
-    ordinal_pie(d)
+    frequency_pie(d)
