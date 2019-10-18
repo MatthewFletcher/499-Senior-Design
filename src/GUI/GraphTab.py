a71b4168003545ad8154ef0922c2063338c21fd4
Sameera <sfw0007@uah.edu>
2019-10-18 13:31:15 -0500

    Got all buttons to work and present graph depending on selection. Can save PNG file of graph

--- src/GUI/GraphTab.py
+++ src/GUI/GraphTab.py
@@ -1,12 +1,14 @@
 from PyQt5.QtWidgets import (QWidget, QVBoxLayout, QLabel, QRadioButton,
-                             QGroupBox, QPushButton, QGridLayout, QSizePolicy, QButtonGroup)
+                             QGroupBox, QPushButton, QGridLayout, QSizePolicy, QButtonGroup, QFileDialog)
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
 from pathlib import Path
 import sys, os
 sys.path.append(str(Path(os.getcwd()).joinpath("../csvtools").resolve()))
 import CSV_Wizard
-import UserSelect
+import random
+# import UserSelect
 
 class GraphTab(QWidget):
     def __init__(self):
@@ -25,7 +27,8 @@ class GraphTab(QWidget):
     def createGraphGroup(self):
         self.GraphGroup = QGroupBox()
 
-        self.myGraph = PlotCanvas(self, width=5, height=4)
+        self.figure = plt.figure()
+        self.myGraph = FigureCanvas(self.figure)
 
         self.GraphGroup.setFixedWidth(1750)
         self.layout = QVBoxLayout()
@@ -50,13 +53,13 @@ class GraphTab(QWidget):
         self.typeGroup.addButton(self.pieRadioButton)
         self.typeGroup.addButton(self.lineRadioButton)
 
-        #self.typeGroup.clicked.connect(self.process())
+        # self.pieRadioButton.setEnabled(False)
 
         # Buttons to let the user submit the data
         self.graphButton = QPushButton("Graph")
         self.graphButton.setDefault(True)
         self.graphButton.setFixedWidth(680)
-        # self.graphButton.clicked.connect(self.graphButtonClicked)
+        self.graphButton.clicked.connect(self.graphButtonClicked)
 
         # Button to let the user save the graph as a PNG file
         self.PNGButton = QPushButton("Save as PNG")
@@ -81,57 +84,33 @@ class GraphTab(QWidget):
         self.CustomGroup.setLayout(self.layout)
 
     # Call this function when the graph button is clicked
-    # def graphButtonClicked(self):
+    def graphButtonClicked(self):
+        masterDF = CSV_Wizard.openFile("../../TestData/IntervalDataTest.csv")
+        # contains numpy array
+        d = masterDF[0]
+        self.figure.clear()
+        if self.vbarRadioButton.isChecked() == True:
+            self.verticalBarGraph(d)
+        elif self.hbarRadioButton.isChecked() == True:
+            self.horizontalBarGraph(d)
+        elif self.pieRadioButton.isChecked() == True:
+            self.ordinal_pie(d)
+        elif self.lineRadioButton.isChecked() == True:
+            self.lineGraph(d)
+
+        self.myGraph.draw()
+        self.repaint()
 
     def PNGButtonClicked(self):
-        self.savePNG()
+        self.saveFileDialog()
 
-    def savePNG(self):
-        self.savefig('graph.png')
-
-    def process(self):
-        if self.vbarRadioButton.isChecked() == True:
-            graph = PlotCanvas.verticalBarGraph()
-        elif self.hbarRadioButton.isChecked() == True:
-            graph2 = PlotCanvas.horizontalBarGraph()
-        elif self.pieRadioButton.isChecked() == True:
-            graph3 = PlotCanvas.ordinal_pie()
-        elif self.lineRadioButton.isChecked() == True:
-            graph4 = PlotCanvas.lineGraph()
-
-class PlotCanvas(FigureCanvas):
-
-    def __init__(self, parent=None, width=5, height=4, dpi=100):
-        fig = Figure(figsize=(width, height), dpi=dpi)
-        self.axes = fig.add_subplot(111)
-
-        FigureCanvas.__init__(self, fig)
-        self.setParent(parent)
-
-        FigureCanvas.setSizePolicy(self,
-                QSizePolicy.Expanding,
-                QSizePolicy.Expanding)
-        FigureCanvas.updateGeometry(self)
-        self.plot()
-
-    def plot(self):
-        myinfo = CSV_Wizard.openFile("../../TestData/IntervalDataTest.csv")
-        # contains numpy array
-        d = myinfo[0]
-        self.horizontalBarGraph(d)
-
-    def lineGraph(self, df):
-        plot = self.figure.add_subplot(111)
-        headers = list(df.columns.values)
-
-        x = df[headers[1]]
-        y = df[headers[2]]
-        plot.set_xlabel(headers[1])
-        plot.set_ylabel(headers[2])
-
-        # Plot the points using matplotlib
-        plot.plot(x, y)
-        self.draw()
+    def saveFileDialog(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getSaveFileName(self, "QFileDialog.getSaveFileName()", "",
+                                                  "All Files (*);;PNG Files (*.png)", options=options)
+        if fileName:
+            self.figure.savefig(fileName)
 
     # This function will graph the frequency or interval data as a horizontal bar graph
     def horizontalBarGraph(self, df):
@@ -144,7 +123,18 @@ class PlotCanvas(FigureCanvas):
             y.append(df[header].sum())
 
         plot.barh(x, y)
-        self.draw()
+
+    def lineGraph(self, df):
+        plot = self.figure.add_subplot(111)
+        headers = list(df.columns.values)
+
+        x = df[headers[1]]
+        y = df[headers[2]]
+        plot.set_xlabel(headers[1])
+        plot.set_ylabel(headers[2])
+
+        # Plot the points using matplotlib
+        plot.plot(x, y)
 
     # This function will graph frequency or interval data as a vertical bar graph
     def verticalBarGraph(self, df):
@@ -157,7 +147,6 @@ class PlotCanvas(FigureCanvas):
             y.append(df[header].sum())
 
         plot.bar(x, y)
-        self.draw()
 
     # This function will graph the frequency or interval data as a pie chart
     def freqint_pie(self, df):
@@ -179,7 +168,6 @@ class PlotCanvas(FigureCanvas):
         plot.pie(y, explode=explode, labels=x, colors=colors,
                 autopct='%1.1f%%', shadow=False, startangle=0)
         plot.axis('equal')
-        self.draw()
 
     # This function will graph ordinal data as a pie chart
     def ordinal_pie(self, df):
@@ -201,5 +189,5 @@ class PlotCanvas(FigureCanvas):
         plot.pie(y, explode=explode, labels=x, colors=colors,
                 autopct='%1.1f%%', shadow=False, startangle=0)
         plot.axis('equal')
-        self.draw()
+
 
