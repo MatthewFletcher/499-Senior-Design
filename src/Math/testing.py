271b0ba63a7dae8f7a5ed68a165a1f2f92ec85fa
Miguel <mao0008@uah.edu>
2019-10-22 15:17:57 -0500

    done with Vector wizard
    format to return dictionary of list of tuples

--- src/Math/testing.py
+++ src/Math/testing.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import CSV_Wizard2 as c
+import CSV_Wizard as c
 import numpy as np
 import Stats_Wizard as s
 import pandas as pd
--- src/csvtools/Vector_Wizard.py
+++ src/csvtools/Vector_Wizard.py
@@ -5,10 +5,16 @@ import statistics as s
 import math as ma
 import scipy.stats as st
 from optparse import OptionParser
-import inspect
+
+import sys, os
+import numpy as np
+from pathlib import Path
+#TODO: GET A BETTER FIX
+#setting the working directory vs code
+#sys.path.append(str(Path(os.getcwd()).joinpath("./src/Math").resolve()))
+#sys.path.append(str(Path(os.getcwd()).joinpath("./src/csvtools").resolve()))
 
 import CSV_Wizard as c
-import numpy as np
 import Stats_Wizard as s
 import pandas as pd
 import inspect
@@ -17,22 +23,20 @@ import inspect
 class DataframeFunction:
     def __init__(self, d):
         self.d = self.vectorSplit(d)
-        #ds=s.Statistics(listarray[1])
 
     def vectorSplit(self,df):
         '''
         Probelm: stat functions are design to take in a list e.g., [1, 2, 3, 4]
         Solutions: This should split dataframe into array of lists for easier management.
         '''
-        headers = list(df.columns.values)
-        xrow, ycol=df.shape
         '''
         Ever column is looped
         '''
         listarray=[]
-        for x in range(ycol):
-            listarray.append(df[headers[x]])
-        listarray=self.vectorS2I(listarray)
+        for column in df:
+            listarray.append(df[column])
+        listarrar=self.vectorS2I(listarray)
+       
         return listarray
     def vectorS2I(self,listarray):
         '''
@@ -42,51 +46,81 @@ class DataframeFunction:
         '''
         Note: even if it is an int numerical value 
         '''
-        for x in range(len(listarray)):
-            check=listarray[x].astype(str).str.isnumeric()
-  
+        for column in listarray:
+            check=column.astype(str).str.isnumeric()
             if check.all():
-                listarray[x]=listarray[x].astype(np.int64)
-            
-                #listarray[x]=listarray[x].astype(np.int64)
-        
-            '''Note: String non numerical value left alone. e.g., Sample 1, Sample 2, ..., etc. '''
+                column=column.astype(np.int64)
+       
+        '''Note: String non numerical value left alone. e.g., Sample 1, Sample 2, ..., etc. '''
 
         return listarray
 
     def getFunctionsStat(self,listFunc):
         #listofdata d
         print("getfuncstat")
-       
-        for x in range(len(self.d)):
-            
-            check=self.d[x].astype(str).str.isnumeric()
+        #item i in list:
+        #python best practice
+        #for row in self.d:
+        #TODO: CHANGE FOR LOOP FOR BETTER USE OF PYTHON--DONE
+      
+        dictlist=[]
+        testdictlist={}
+
+        for data in self.d:
+            check=data.astype(str).str.isnumeric()
             if(check.all()):
-                print(self.d[x])
-                for y in range(len(listFunc)):
-                    clssObj=s.Statistics(self.d[x])
-                    method=listFunc[y]
-                    name=method[0]
-                    addr=method[1]
+                dic={}
+                dic2=[]
+                
+                for func in listFunc:
+                    clssObj=s.Statistics(data)
+                    name=func[0]
+                    addr=func[1]
                     method_to_call=getattr(clssObj,name)
                     result=method_to_call()
-                    print(name, result)
-        
-           
-        
 
+                    dic.update({name:result})
+                    dic2.append(tuple((name,result)))
+                
+                dictlist=np.append(dictlist,dic)
+                testdictlist.update({data.name: dic2})
+
+        #dictionary list of dictionarys
+        print(dictlist,'\n')
+        print(dictlist[0])
+        test=dictlist[0]
+        print(test['s_max'])
+
+        #dictionary list of tuples
+        print('-------')
+        print(testdictlist,'\n')
+        print(testdictlist[' Expected Freq.'])
+        test=testdictlist[' Expected Freq.']
+        print(test[0])
+        test2=test[0]
+        print(test2[1])
+        
+        return testdictlist
+  
+
+'''
+#dictionary list of tuples row {mean: 0}
+cola {mean:0} {max: 0}
+colb {etc..}
+'''     
 
 
 def main():
     df=c.openFile("TestData/FrequencyDataTest.csv")[0]
     print(df)
     print("------")
-    test=DataframeFunction(df)
+    test=DataframeFunction(df)#----
     #if gui side needs to receive the testlist this(below) need to happen
-
     ds=s.Statistics(0)
-    funcpath=ds.test_list()    
-    test.getFunctionsStat(funcpath)
+    funcpath=ds.test_list()
+    #ds.test_list() 
+    #gui-testlist is changed based on user choice   
+    test.getFunctionsStat(funcpath)#-----
     
 
 main()
