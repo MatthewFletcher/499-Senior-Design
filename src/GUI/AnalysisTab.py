563d2e8f3cffae27f1ae31ce6ce0029d8989d4e6
Jada Blankenship <30299670+jada4569@users.noreply.github.com>
2019-10-22 22:23:27 -0500

    AnalysisTab start

--- src/GUI/AnalysisTab.py
+++ src/GUI/AnalysisTab.py
@@ -1,4 +1,5 @@
-from PyQt5.QtWidgets import (QApplication, QWidget, QGridLayout, QLabel, QGroupBox)
+from PyQt5.QtWidgets import (QApplication, QWidget, QGridLayout, QLabel, QGroupBox,
+                             QListWidget)
 import sys
 
 
@@ -10,13 +11,43 @@ class AnalysisTab(QWidget):
         self.screen = self.app.primaryScreen()
         self.size = self.screen.size()
 
-        self.tableWidth = self.size.width() * 0.65
-        self.customWidth = self.size.width() * 0.3
+        self.createTextGroup()
+        self.createChooseGroup()
 
+        self.layout = QGridLayout()
+        self.layout.addWidget(self.TextGroup, 0, 0, 0, 1)
+        self.layout.addWidget(self.ChooseGroup, 0, 1)
+        self.setLayout(self.layout)
         self.show()
 
-        # The right side of DataTab containing Radio Buttons and
-        # text boxes for user input on how they want their graph
-        def createCustomGroup(self):
-            self.CustomGroup = QGroupBox()
-            self.setStyleSheet("font: 15pt Tw Cen MT")
+    # The left side of AnalysisTab containing the textbox
+    # where the analysis will be shown
+    def createTextGroup(self):
+        self.TextGroup = QGroupBox()
+        self.setStyleSheet("font: 15pt Tw Cen MT")
+
+        # Here is where the analysis will go
+        self.analysis = QLabel()
+
+        self.layout = QGridLayout()
+        self.layout.addWidget(self.analysis)
+        self.TextGroup.setLayout(self.layout)
+
+    # The right side of AnalysisTab containing the buttons for
+    # analysis
+    def createChooseGroup(self):
+        self.ChooseGroup = QGroupBox()
+        self.setStyleSheet("font: 15pt Tw Cen MT")
+
+        # List widgets where users will choose what analysis
+        # they would like ran on their data
+        self.intervalAnalysis = QListWidget()
+        self.ordinalAnalysis = QListWidget()
+        self.frequencyAnalysis = QListWidget()
+
+        self.intervalAnalysis.
+
+
+
+        self.layout = QGridLayout()
+        self.ChooseGroup.setLayout(self.layout)
