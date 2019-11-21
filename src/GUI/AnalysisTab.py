551e77cbbf1ba5ad938bbd9bf1bb2ec726e2d315
Jada Blankenship <30299670+jada4569@users.noreply.github.com>
2019-11-21 17:28:57 -0600

    commented out pixmap adjustment

--- src/GUI/AnalysisTab.py
+++ src/GUI/AnalysisTab.py
@@ -25,8 +25,8 @@ class AnalysisTab(QWidget):
         super().__init__()
         if hasattr(Qt, 'AA_EnableHighDpiScaling'):
             QApplication.setAttribute(Qt.AA_EnableHighDpiScaling, True)
-        if hasattr(Qt, 'AA_UseHighDpiPixmaps'):
-            QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps, True)
+        # if hasattr(Qt, 'AA_UseHighDpiPixmaps'):
+        #     QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps, True)
 
         self.app = QApplication(sys.argv)
         self.screen = self.app.primaryScreen()
